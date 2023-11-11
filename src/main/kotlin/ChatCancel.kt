dc206b61b9f6721c18978acaf3f5ef46e317fc91
Nicholas Stonecipher <nickster258@users.noreply.github.com>
2023-11-11 12:16:02 -0500

    Disable join/leave messages

--- src/main/kotlin/ChatCancel.kt
+++ src/main/kotlin/ChatCancel.kt
@@ -1,12 +1,24 @@
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class ChatListener : Listener {
     @EventHandler
     fun onChatEvent(event: AsyncPlayerChatEvent) {
         event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onJoinEvent(event: PlayerJoinEvent) {
+        event.joinMessage = null
+    }
+
+    @EventHandler
+    fun onLeaveEvent(event: PlayerQuitEvent) {
+        event.quitMessage = null
     }
 }
 
